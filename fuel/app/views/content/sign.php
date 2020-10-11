86fcf98e24fc40f4019b1677fade1f935da48ef1
MassMatsu <masa10matsukawa@gmail.com>
2020-10-11 22:32:38 +0900

    working on views content/sign

--- fuel/app/views/content/sign.php
+++ fuel/app/views/content/sign.php
@@ -1,4 +1,10 @@
-<div>
-  here is the content!!
-  <?php //echo $data; ?>
+<div class="ctn-main">
+  <section class="ctn-form">
+    <h1>ユーザー登録</h1>
+
+    
+    <?php echo $signupform ?>
+    <?php //echo $data; ?>
+  </section>
+  
 </div>
